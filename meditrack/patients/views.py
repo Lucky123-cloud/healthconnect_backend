2c922997e7bd13bb5bc358b916a2799f4b5acff2
Lucky-cloud 123 <luckybaraka21@gmail.com>
2025-10-31 14:30:41 +0300

    added the views and templates

--- meditrack/patients/views.py
+++ meditrack/patients/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Patient, Doctor, Appointment
+from django.utils import timezone
 
-# Create your views here.
+def home(request):
+    return render(request, 'patients/home.html')
+
+def patient_list(request):
+    patients = Patient.objects.all()
+    return render(request, 'patients/patients.html', {'patients': patients})
+
+def appointments(request):
+    doctors = Doctor.objects.filter(available=True)
+    patients = Patient.objects.all()
+    appointments = Appointment.objects.select_related('patient', 'doctor')
+
+    if request.method == "POST":
+        patient_id = request.POST.get('patient')
+        doctor_id = request.POST.get('doctor')
+        Appointment.objects.create(
+            patient_id=patient_id,
+            doctor_id=doctor_id,
+            date=timezone.now(),
+            confirmed=True
+        )
+        return redirect('appointments')
+
+    return render(request, 'patients/appointments.html', {
+        'doctors': doctors,
+        'patients': patients,
+        'appointments': appointments
+    })
